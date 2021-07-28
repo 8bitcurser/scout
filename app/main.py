5095df173d67cd41e7de79b82e302b5ad1f73fda
Tom <tomowl@pm.me>
2021-07-27 23:09:17 -0300

    Add jinja2 templating and uvicorn proper running configuration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
+
+templates = Jinja2Templates(directory="app/templates")
 app = FastAPI()
 
 
-@app.get("/{name}")
-def root(name: str = "World"):
-    return {"Welcome": f"{name}"}
+@app.get("/")
+async def root(req: Request):
+    template = templates.TemplateResponse(
+        "index.html",
+        {'request': req}
+    )
+
+    return template
