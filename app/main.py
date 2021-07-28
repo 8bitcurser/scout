532966f9bb5af3413c8fa6c27cf526e20fa03053
Tom <tomowl@pm.me>
2021-07-27 23:45:18 -0300

    Add statics

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ from typing import Optional
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 templates = Jinja2Templates(directory="app/templates")
+
 app = FastAPI()
 
+app.mount("/statics", StaticFiles(directory="statics"), name="statics")
 
 @app.get("/")
 async def root(req: Request):
---
+++ app/statics/app.js
@@ -0,0 +1,5 @@
+const app = new Vue({
+    el: '#app',
+    data: {'message': 'Scout V0.0.1'},
+    methods: {},
+})
