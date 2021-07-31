c64244fd516a0c058ebf1a7216b6363377dbef8d
Tom <tomowl@pm.me>
2021-07-31 01:42:09 -0300

    Add sample component for CORS interaction + CORS configuration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ScoutRoot />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ScoutRoot from './components/ScoutRoot.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ScoutRoot
   }
 }
 </script>
---
+++ frontend/src/components/ScoutRoot.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="scoutRoot">
+        <button v-on:click="load">Request Root</button>
+        <span id="msg">{{ msg }}</span>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: 'ScoutRoot',
+    data: function() {
+        return {msg: 'Default!'}
+    },
+    methods: {
+        load () {
+            axios.get('http://localhost:8000/').then(
+                (response) => {
+                    this.msg = response.data;
+                }
+            )
+        }   
+    }    
+}
+</script>
+
+<style scoped>
+.scoutRoot {
+    background-color: black;
+    color: white;
+}
+#msg {
+    display: block;
+}
+</style>
