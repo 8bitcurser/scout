8314a3d2a984560cd7f8d0dde4d2e02619c86163
Tom <tomowl@pm.me>
2021-08-04 00:21:02 -0300

    Sanitize code

--- backend/integrations/sushi.py
+++ backend/integrations/sushi.py
@@ -6,11 +6,37 @@ class Sushi:
     def __init__(self, *args, **kwargs):
         self.base_url = 'https://api2.sushipro.io?action={}'
 
-    def get_all_pairs(self):
+    def get_pairs(self, token_1, token_2):
         all_pairs_url = self.base_url.format('all_pairs')
         res = get(all_pairs_url)
         if res.status_code == 200:
-            ret = res.json()[1]
+            sushi_pairs = res.json()[1]
+            # Pairs in uniswap are not necessarily generated in a particular order
+            # therefor usdt/usdc could be swapped into usdc/usdt but only 1 pair with
+            # both contracts will exist.
+            look_pair = [
+                pair for pair in sushi_pairs
+                if (
+                    pair['Token_1_symbol'].lower() == token_1 and
+                    pair['Token_2_symbol'].lower() == token_2
+                ) or (
+                    pair['Token_2_symbol'].lower() == token_1 and
+                    pair['Token_1_symbol'].lower() == token_2
+                ) 
+            ]
+
+            if look_pair:
+                if len(look_pair) == 1:
+                    # As pairs are not necessarily in the correct order the prices aren't either
+                    # we need to divide in the proper order.
+                    if look_pair[0]["Token_1_symbol"].lower() == token_1:
+                        sushi_res = look_pair[0]["Token_1_price"] / look_pair[0]["Token_2_price"]
+                    else:
+                        sushi_res = look_pair[0]["Token_2_price"] / look_pair[0]["Token_1_price"]
+                else:
+                    sushi_res = "Too many results"
+            else:
+                sushi_res = None
         else:
-            ret = []
-        return ret
+            sushi_res = None
+        return sushi_res
--- backend/main.py
+++ backend/main.py
@@ -47,36 +47,13 @@ async def price(token_1: str, token_2: str):
     uni_res = uni_res / min_unit_of_token_multiplier
     # Sushiswap obtention
     sushi = Sushi()
-    sushi_pairs = sushi.get_all_pairs()
-
-    # Pairs in uniswap are not necessarily generated in a particular order
-    # therefor usdt/usdc could be swapped into usdc/usdt but only 1 pair with
-    # both contracts will exist.
-    look_pair = [
-        pair for pair in sushi_pairs
-        if (
-            pair['Token_1_symbol'].lower() == token_1.lower() and
-            pair['Token_2_symbol'].lower() == token_2.lower()
-        ) or (
-            pair['Token_2_symbol'].lower() == token_1.lower() and
-            pair['Token_1_symbol'].lower() == token_2.lower()
-        ) 
-    ]
-
-    if look_pair:
-        if len(look_pair) == 1:
-            # As pairs are not necessarily in the correct order the prices aren't either
-            # we need to divide in the proper order.
-            if look_pair[0]["Token_1_symbol"].lower() == token_1.lower():
-                sushi_res = look_pair[0]["Token_1_price"] / look_pair[0]["Token_2_price"]
-            else:
-                sushi_res = look_pair[0]["Token_2_price"] / look_pair[0]["Token_1_price"]
-        else:
-            sushi_res = "Too many results"
-    else:
-        sushi_res = None
-
+    sushi_res = sushi.get_pairs(
+        token_1.lower(),
+        token_2.lower()
+    )
+    # Pancakeswap obtention
     pancake = Pancake()
+    # pancake swap works with bep20 addresses we need to find those first.
     token_1_bep_addr = pancake.get_token(token_1.lower())
     token_2_bep_addr = pancake.get_token(token_2.lower())
     pancake_res = pancake.get_pair(token_1_bep_addr, token_2_bep_addr)
