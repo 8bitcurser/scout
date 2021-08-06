cd5659decfadad7291abc5d64145e369ca094f49
Tom <tomowl@pm.me>
2021-08-06 01:00:48 -0300

    Add integratio with etherscan through beautiful soup as to obtain the token addresses.

---
+++ backend/integrations/etherscan.py
@@ -0,0 +1,79 @@
+from re import search
+
+from bs4 import BeautifulSoup
+from requests import get
+
+
+class Ether:
+    '''Etherscan adaptor.'''
+
+    def __init__(self, *args, **kwargs):
+        self.base_url = 'https://etherscan.io/tokens?p={}'
+        self.headers = {
+            "authority": "etherscan.io",
+            "cache-control": "max-age=0",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.115 Safari/537.36",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-gpc": "1",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-user": "?1",
+            "sec-fetch-dest": "document",
+            "referer": "https://etherscan.io/tokens",
+            "accept-language": "en-US,en;q=0.9",
+            "cookie": "ASP.NET_SessionId=kqmzjmcfnxbowxxybaa1geih; __cflb=02DiuFnsSsHWYH8WqVXaqGvd6BSBaXQLTRKLTJUZ53xse; __stripe_mid=f5edd8f6-550e-49ed-bdee-b43aabfcbdd7a29e02"
+        }
+        self.anchors = []
+        self.tokens = {}
+
+    def get_page(self, num=1):
+        """Retrieves a etherscan page as a Soup object."""
+        response = get(
+            self.base_url.format(num),
+            headers=self.headers
+        )
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            return soup
+        else:
+            return None
+    
+    def get_anchors(self, soup) -> list:
+        self.anchors = soup.find_all('a')
+    
+    def get_max_pages(self) -> int:
+        all_anchors = self.anchors
+        link_anchor = [
+            anchor for anchor in all_anchors if 'page-link' in anchor.attrs.get('class', [])
+            and 'tokens?p' in anchor['href']
+        ]
+        max_page = max(
+            map(int, [anchor['href'].replace('tokens?p=', '') for anchor in link_anchor])
+        )
+
+        return max_page
+
+    def get_tokens_from_page(self):
+        all_anchors = self.anchors
+        token_anchor = [
+            anchor for anchor in all_anchors if 'token' in anchor['href']
+            and 'text-primary' in anchor.attrs.get('class', [])
+        ]
+        # data from tokens comes in format "name of token (symbol of token)"
+        # the regex retrieves only the symbol of the token
+        for token in token_anchor:
+            symbol = search(r'(?<=\()[\w\d]+', token.text)
+            if symbol is not None:
+                symbol = symbol.group(0)
+                address = token['href'].replace('/token/', '')
+                self.tokens.update(
+                    {
+                       symbol: {
+                           'address': address,
+                           'decimals': 0,
+                       } 
+                    }
+                )
+            else:
+                print(f"Could not retrieve possible token: {token.text}")
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
-from asyncio import create_task
+from json import dumps, loads
+from re import search
+from time import sleep
+
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from requests import get
 
-from helpers import token_addresses
-
+from integrations.etherscan import Ether
 from integrations.pancake import Pancake
 from integrations.sushi import Sushi
 from integrations.uni import Uni
@@ -24,9 +27,35 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+
 @app.get("/")
-async def root(req: Request):
-    return req.json
+async def seed_tokens():
+    ether = Ether()
+    main_page = ether.get_page()
+    if main_page is not None:
+        ether.get_anchors(main_page)
+        max_page = ether.get_max_pages()
+        pages = list(range(2, max_page+1))
+        ether.get_tokens_from_page()
+        for page in pages:
+            page = ether.get_page(num=page)
+            ether.get_anchors(page)
+            if page is not None:
+                ether.get_tokens_from_page()
+                sleep(0.5)
+            else:
+                ret = "Could not retrieve information from etherscan.io"
+                break
+
+        with open('fixtures_tokens.json', 'w') as fix:
+            fix.write(dumps(ether.tokens, sort_keys=True, indent=4))
+        
+        ret = "Succesfully seeded platform with tokens"
+        
+    else:
+        ret = 'Could not retrieve information from etherscan.io'
+
+    return {'data': ret}
 
 
 @app.get('/price/{token_1}/{token_2}')
@@ -34,36 +63,44 @@ async def price(token_1: str, token_2: str):
     """Return exchange price for two given tickers on different platforms."""
     token_1 = token_1.lower()
     token_2 = token_2.lower()
-    token_1_settings = token_addresses.get(token_1, '')
-    token_2_settings = token_addresses.get(token_2, '')
-    token_1_addr = token_1_settings[0]
-    token_2_addr = token_2_settings[0]
-    # for more details on this look at
-    # https://github.com/uniswap-python/uniswap-python/issues/12
-    # and https://github.com/uniswap-python/uniswap-python/issues/41
-    min_unit_of_token_multiplier = token_1_settings[1]
-    # Uniswap obtention
-    uni = Uni()
-    uni_res = await uni.get_pair_price(
-        token_1_addr, token_2_addr, min_unit_of_token_multiplier
-    )
-    # Sushiswap obtention
-    sushi = Sushi()
-    sushi_res = await sushi.get_pairs(token_1, token_2)
 
-    # Pancakeswap obtention
-    pancake = Pancake()
-    # pancake swap works with bep20 addresses we need to find those first.
-    token_1_bep_addr = pancake.get_token(token_1)
-    token_2_bep_addr = pancake.get_token(token_2)
-    pancake_res = await pancake.get_pair(token_1_bep_addr, token_2_bep_addr)
-    
-    # conversion to decimal unit for uniswap response
-    uni_res = uni_res / min_unit_of_token_multiplier
-    
-    res = {
-        'uniswap': f'{token_1} vs {token_2} = {uni_res}',
-        'sushiswap': f'{token_1} vs {token_2} = {sushi_res}',
-        'pancakeswap': f'{token_1} vs {token_2} = {pancake_res}'
-    }
+    sushi = Sushi()
+    uni = Uni()
+    pancake = Pancake()    
+
+    with open('fixtures_tokens.json', 'r') as fix:
+        sanitized_addresses_dict = loads(fix.read())
+
+    if sanitized_addresses_dict:
+        token_1_settings = sanitized_addresses_dict.get(token_1, {})
+        token_2_settings = sanitized_addresses_dict.get(token_2, {})
+
+        token_1_addr = token_1_settings.get('contract')
+        token_2_addr = token_2_settings.get('contract')
+        # for more details on this look at
+        # https://github.com/uniswap-python/uniswap-python/issues/12
+        # and https://github.com/uniswap-python/uniswap-python/issues/41
+        min_unit_of_token_multiplier = 10**token_1_settings.get('decimals')
+        # Uniswap obtention
+        uni_res = await uni.get_pair_price(
+            token_1_addr, token_2_addr, min_unit_of_token_multiplier
+        )
+        # Sushiswap obtention    
+        sushi_res = await sushi.get_pairs(token_1, token_2)
+
+        # Pancakeswap obtention
+        # pancake swap works with bep20 addresses we need to find those first.
+        token_1_bep_addr = pancake.get_token(token_1)
+        token_2_bep_addr = pancake.get_token(token_2)
+        pancake_res = await pancake.get_pair(token_1_bep_addr, token_2_bep_addr)
+        
+        # conversion to decimal unit for uniswap response
+        uni_res = uni_res / min_unit_of_token_multiplier
+        res = {
+            'uniswap': f'{token_1} vs {token_2} = {uni_res}',
+            'sushiswap': f'{token_1} vs {token_2} = {sushi_res}',
+            'pancakeswap': f'{token_1} vs {token_2} = {pancake_res}'
+        }
+    else:
+        res = {'data': 'Could not retrieve tokens, try calling the root endpoint "/"'}
     return res
